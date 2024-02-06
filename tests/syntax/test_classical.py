f0d31e49040647e79587a0ff50f43c85abadf6fe
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-02-06 18:43:35 -0300

    Generalize the test cases for the basic integer syntax

--- tests/syntax/test_classical.py
+++ tests/syntax/test_classical.py
@@ -1,24 +1,59 @@
 import pytest
 
-from qoala.types.classical.integer import Int32, UInt32, Int
+from typing import Generic, TypeVar
+
+from qoala.types.classical.integer import Int32, UInt32, Int, IntegerType
+
+_Type = TypeVar("_Type", bound=IntegerType)
 
 
-class TestClassicalSyntax:
-    def test_int32_creation(self):
-        value_a = Int32(0)
-        assert isinstance(value_a, Int32)
+@pytest.mark.parametrize("clazz", (Int32, UInt32, Int))
+class TestClassicalSyntax(Generic[_Type]):
+    # These two methods are helpful to create the right type of integer, by
+    # _statically_ calling the constructor depending on the name of the class.
+    # It deviates a bit from what a user could write (a user will never use a
+    # factory based on a parametric type), but it simplifies the specification
+    # of all the possible test cases.
+
+    @staticmethod
+    def _get_int_from_immediate(clazz: type, immediate: int) -> _Type:
+        match clazz.__name__:
+            case "Int32":
+                return Int32(immediate=immediate)
+            case "UInt32":
+                return UInt32(immediate=immediate)
+            case "Int":
+                return Int(immediate=immediate)
+            case _:
+                raise NotImplementedError()
+
+    @staticmethod
+    def _get_int_from_same_type(clazz: type, other_int: _Type) -> _Type:
+        match clazz.__name__:
+            case "Int32":
+                return Int32(other_int32=other_int)
+            case "UInt32":
+                return UInt32(other_uint32=other_int)
+            case "Int":
+                return Int(other_int32=other_int)
+            case _:
+                raise NotImplementedError()
+
+    def test_int_creation(self, clazz: type):
+        value_a = TestClassicalSyntax._get_int_from_immediate(clazz, 0)
+        assert isinstance(value_a, clazz)
         #assert value_a.get_value() == 0
 
-    def test_int32_creation_from_other_int32(self):
-        value_a = Int32(immediate=10)
-        value_b = Int32(other_int32=value_a)
-        assert isinstance(value_b, Int32)
+    def test_int_creation_from_other_int32(self, clazz: type):
+        value_a = TestClassicalSyntax._get_int_from_immediate(clazz, 10)
+        value_b = TestClassicalSyntax._get_int_from_same_type(clazz, value_a)
+        assert isinstance(value_b, clazz)
         #assert value_b.get_value() == 10
 
     @pytest.mark.skip(reason="Int32 - Overloaded operators do not return anything just yet")
-    def test_int32_operator_overload_correctness(self):
-        value_a = Int32(immediate=2)
-        value_b = Int32(immediate=6)
+    def test_int_operator_overload_correctness(self, clazz: type):
+        value_a = TestClassicalSyntax._get_int_from_immediate(clazz, 2)
+        value_b = TestClassicalSyntax._get_int_from_immediate(clazz, 6)
         value_c = value_a + value_b
         value_d = value_b + value_a
         value_e = value_b - value_a
@@ -29,104 +64,16 @@ class TestClassicalSyntax:
         value_j = value_a / value_b
 
         # Assert closure correctness
-        assert isinstance(value_c, Int32)
-        assert isinstance(value_d, Int32)
-        assert isinstance(value_e, Int32)
-        assert isinstance(value_f, Int32)
-        assert isinstance(value_g, Int32)
-        assert isinstance(value_h, Int32)
-        assert isinstance(value_i, Int32)
-        assert isinstance(value_j, Int32)
+        assert isinstance(value_c, clazz)
+        assert isinstance(value_d, clazz)
+        assert isinstance(value_e, clazz)
+        assert isinstance(value_f, clazz)
+        assert isinstance(value_g, clazz)
+        assert isinstance(value_h, clazz)
+        assert isinstance(value_i, clazz)
+        assert isinstance(value_j, clazz)
 
-        # Assert operation application correcteness
-        #assert value_c.get_value() == 8
-        #assert value_d.get_value() == 8
-        #assert value_e.get_value() == 4
-        #assert value_f.get_value() == -4
-        #assert value_g.get_value() == 12
-        #assert value_h.get_value() == 12
-        #assert value_i.get_value() == 3
-        #assert value_j.get_value() == 0
-
-    def test_uint32_creation(self):
-        value_a = UInt32(0)
-        assert isinstance(value_a, UInt32)
-        #assert value_a.get_value() == 0
-
-    def test_uint32_creation_from_other_int32(self):
-        value_a = UInt32(immediate=10)
-        value_b = UInt32(other_uint32=value_a)
-        assert isinstance(value_b, UInt32)
-        #assert value_b.get_value() == 10
-
-    @pytest.mark.skip(reason="UInt32 - Overloaded operators do not return anything just yet")
-    def test_uint32_operator_overload_correctness(self):
-        value_a = UInt32(immediate=2)
-        value_b = UInt32(immediate=6)
-        value_c = value_a + value_b
-        value_d = value_b + value_a
-        value_e = value_b - value_a
-        value_f = value_a - value_b
-        value_g = value_a * value_b
-        value_h = value_b * value_a
-        value_i = value_b / value_a
-        value_j = value_a / value_b
-
-        # Assert closure correctness
-        assert isinstance(value_c, UInt32)
-        assert isinstance(value_d, UInt32)
-        assert isinstance(value_e, UInt32)
-        assert isinstance(value_f, UInt32)
-        assert isinstance(value_g, UInt32)
-        assert isinstance(value_h, UInt32)
-        assert isinstance(value_i, UInt32)
-        assert isinstance(value_j, UInt32)
-
-        # Assert operation application correcteness
-        #assert value_c.get_value() == 8
-        #assert value_d.get_value() == 8
-        #assert value_e.get_value() == 4
-        #assert value_f.get_value() == -4
-        #assert value_g.get_value() == 12
-        #assert value_h.get_value() == 12
-        #assert value_i.get_value() == 3
-        #assert value_j.get_value() ==
-
-    def test_int_creation(self):
-        value_a = Int(0)
-        assert isinstance(value_a, Int)
-        #assert value_a.get_value() == 0
-
-    def test_int_creation_from_other_int32(self):
-        value_a = Int(immediate=10)
-        value_b = Int(other_int32=value_a)
-        assert isinstance(value_b, Int)
-        #assert value_b.get_value() == 10
-
-    @pytest.mark.skip(reason="Int32 - Overloaded operators do not return anything just yet")
-    def test_int_operator_overload_correctness(self):
-        value_a = Int(immediate=2)
-        value_b = Int(immediate=6)
-        value_c = value_a + value_b
-        value_d = value_b + value_a
-        value_e = value_b - value_a
-        value_f = value_a - value_b
-        value_g = value_a * value_b
-        value_h = value_b * value_a
-        value_i = value_b / value_a
-        value_j = value_a / value_b
-
-        # Assert closure correctness
-        assert isinstance(value_c, Int)
-        assert isinstance(value_d, Int)
-        assert isinstance(value_e, Int)
-        assert isinstance(value_f, Int)
-        assert isinstance(value_g, Int)
-        assert isinstance(value_h, Int)
-        assert isinstance(value_i, Int)
-        assert isinstance(value_j, Int)
-
-        # Assert operation application correcteness
+        # Assert operation application correctness
         #assert value_c.get_value() == 8
         #assert value_d.get_value() == 8
         #assert value_e.get_value() == 4
