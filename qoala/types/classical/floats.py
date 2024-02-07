f36f56c48547d36a716d2f852c2a13f6948305e7
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-02-07 12:21:26 -0300

    Add skeleton code and tests for floating point types

---
+++ qoala/types/classical/floats.py
@@ -0,0 +1,110 @@
+from abc import ABC, abstractmethod
+from typing import Self
+
+from qoala.types.classical import QoalaClassicalType, _Internal_Value_Type
+
+
+class FloatingPointType(QoalaClassicalType[_Internal_Value_Type], ABC):
+    # Operations associated with all float types
+    @abstractmethod
+    def add(self, other: Self) -> Self:
+        ...
+
+    @abstractmethod
+    def subtract(self, other: Self) -> Self:
+        ...
+
+    @abstractmethod
+    def multiply(self, other: Self) -> Self:
+        ...
+
+    @abstractmethod
+    def divide(self, other: Self) -> Self:
+        ...
+
+    # Method used for operator overload
+    def __add__(self, other: Self) -> Self:
+        return self.add(other)
+
+    def __sub__(self, other: Self) -> Self:
+        return self.subtract(other)
+
+    def __mul__(self, other: Self) -> Self:
+        return self.multiply(other)
+
+    def __truediv__(self, other: Self) -> Self:
+        return self.divide(other)
+
+
+# TODO - Change the `float` parametric type when implementing
+#        the internal representation.
+# TODO - Expose the symbol of the internal representation
+#        of this type
+class Float(FloatingPointType[float]):
+    internal_representation: float
+
+    def __init__(
+            self,
+            immediate: float = 0,
+            other_float: Self = 0,
+            # TODO - The next arguments are used when creating a Float from other types
+    ):
+        # TODO - Implement the internal representation and storage of the Float
+        pass
+
+    def _get_value(self) -> _Internal_Value_Type:
+        # TODO - Implement
+        pass
+
+    def add(self, other: Self) -> Self:
+        # TODO - Implement
+        pass
+
+    def subtract(self, other: Self) -> Self:
+        # TODO - Implement
+        pass
+
+    def multiply(self, other: Self) -> Self:
+        # TODO - Implement
+        pass
+
+    def divide(self, other: Self) -> Self:
+        # TODO - Implement
+        pass
+
+
+# TODO - Change the `float` parametric type when implementing
+#        the internal representation.
+# TODO - Expose the symbol of the internal representation
+#        of this type
+class Double(FloatingPointType[float]):
+    internal_representation: float
+
+    def __init__(
+            self,
+            immediate: float = 0,
+            other_double: Self = 0,
+            # TODO - The next arguments are used when creating a Double from other types
+    ):
+        # TODO - Implement the internal representation and storage of the Double
+        pass
+
+    def _get_value(self) -> _Internal_Value_Type:
+        # TODO - Implement
+        pass
+
+    def add(self, other: Self) -> Self:
+        # TODO - Implement
+        pass
+
+    def subtract(self, other: Self) -> Self:
+        # TODO - Implement
+        pass
+
+    def multiply(self, other: Self) -> Self:
+        # TODO - Implement
+        pass
+
+    def divide(self, other: Self) -> Self:
+        # TODO - Implement
+        pass
--- tests/syntax/test_classical.py
+++ tests/syntax/test_classical.py
@@ -1,14 +1,16 @@
-import pytest
-
 from typing import Generic, TypeVar
 
+import pytest
+
+from qoala.types.classical.floats import Float, Double, FloatingPointType
 from qoala.types.classical.integer import Int32, UInt32, Int, IntegerType
 
-_Type = TypeVar("_Type", bound=IntegerType)
+_Base_Type_Int = TypeVar("_Base_Type_Int", bound=IntegerType)
+_Base_Type_Float = TypeVar("_Base_Type_Float", bound=FloatingPointType)
 
 
 @pytest.mark.parametrize("clazz", (Int32, UInt32, Int))
-class TestClassicalSyntax(Generic[_Type]):
+class TestIntegerClassicalSyntax(Generic[_Base_Type_Int]):
     # These two methods are helpful to create the right type of integer, by
     # _statically_ calling the constructor depending on the name of the class.
     # It deviates a bit from what a user could write (a user will never use a
@@ -16,7 +18,7 @@ class TestClassicalSyntax(Generic[_Type]):
     # of all the possible test cases.
 
     @staticmethod
-    def _get_int_from_immediate(clazz: type, immediate: int) -> _Type:
+    def _get_int_from_immediate(clazz: type, immediate: int) -> _Base_Type_Int:
         match clazz.__name__:
             case "Int32":
                 return Int32(immediate=immediate)
@@ -28,7 +30,7 @@ class TestClassicalSyntax(Generic[_Type]):
                 raise NotImplementedError()
 
     @staticmethod
-    def _get_int_from_same_type(clazz: type, other_int: _Type) -> _Type:
+    def _get_int_from_same_type(clazz: type, other_int: _Base_Type_Int) -> _Base_Type_Int:
         match clazz.__name__:
             case "Int32":
                 return Int32(other_int32=other_int)
@@ -40,20 +42,87 @@ class TestClassicalSyntax(Generic[_Type]):
                 raise NotImplementedError()
 
     def test_int_creation(self, clazz: type):
-        value_a = TestClassicalSyntax._get_int_from_immediate(clazz, 0)
+        value_a = TestIntegerClassicalSyntax._get_int_from_immediate(clazz, 0)
         assert isinstance(value_a, clazz)
         #assert value_a.get_value() == 0
 
     def test_int_creation_from_other_int32(self, clazz: type):
-        value_a = TestClassicalSyntax._get_int_from_immediate(clazz, 10)
-        value_b = TestClassicalSyntax._get_int_from_same_type(clazz, value_a)
+        value_a = TestIntegerClassicalSyntax._get_int_from_immediate(clazz, 10)
+        value_b = TestIntegerClassicalSyntax._get_int_from_same_type(clazz, value_a)
         assert isinstance(value_b, clazz)
         #assert value_b.get_value() == 10
 
-    @pytest.mark.skip(reason="Int32 - Overloaded operators do not return anything just yet")
+    @pytest.mark.skip(reason="Integers - Overloaded operators do not return anything just yet")
     def test_int_operator_overload_correctness(self, clazz: type):
-        value_a = TestClassicalSyntax._get_int_from_immediate(clazz, 2)
-        value_b = TestClassicalSyntax._get_int_from_immediate(clazz, 6)
+        value_a = TestIntegerClassicalSyntax._get_int_from_immediate(clazz, 2)
+        value_b = TestIntegerClassicalSyntax._get_int_from_immediate(clazz, 6)
+        value_c = value_a + value_b
+        value_d = value_b + value_a
+        value_e = value_b - value_a
+        value_f = value_a - value_b
+        value_g = value_a * value_b
+        value_h = value_b * value_a
+        value_i = value_b / value_a
+        value_j = value_a / value_b
+
+        # Assert closure correctness
+        assert isinstance(value_c, clazz)
+        assert isinstance(value_d, clazz)
+        assert isinstance(value_e, clazz)
+        assert isinstance(value_f, clazz)
+        assert isinstance(value_g, clazz)
+        assert isinstance(value_h, clazz)
+        assert isinstance(value_i, clazz)
+        assert isinstance(value_j, clazz)
+
+        # Assert operation application correctness
+        #assert value_c.get_value() == 8
+        #assert value_d.get_value() == 8
+        #assert value_e.get_value() == 4
+        #assert value_f.get_value() == -4
+        #assert value_g.get_value() == 12
+        #assert value_h.get_value() == 12
+        #assert value_i.get_value() == 3
+        #assert value_j.get_value() == 0
+
+
+@pytest.mark.parametrize("clazz", (Float, Double))
+class TestFloatClassicalSyntax(Generic[_Base_Type_Float]):
+    @staticmethod
+    def _get_float_from_immediate(clazz: type, immediate: float) -> _Base_Type_Float:
+        match clazz.__name__:
+            case "Float":
+                return Float(immediate=immediate)
+            case "Double":
+                return Double(immediate=immediate)
+            case _:
+                raise NotImplementedError()
+
+    @staticmethod
+    def _get_float_from_same_type(clazz: type, other_int: _Base_Type_Float) -> _Base_Type_Float:
+        match clazz.__name__:
+            case "Float":
+                return Float(other_float=other_int)
+            case "Double":
+                return Double(other_double=other_int)
+            case _:
+                raise NotImplementedError()
+
+    def test_float_creation(self, clazz: type):
+        value_a = TestFloatClassicalSyntax._get_float_from_immediate(clazz, 0)
+        assert isinstance(value_a, clazz)
+        #assert value_a.get_value() == 0
+
+    def test_float_creation_from_other_int32(self, clazz: type):
+        value_a = TestFloatClassicalSyntax._get_float_from_immediate(clazz, 10)
+        value_b = TestFloatClassicalSyntax._get_float_from_same_type(clazz, value_a)
+        assert isinstance(value_b, clazz)
+        #assert value_b.get_value() == 10
+
+    @pytest.mark.skip(reason="Floats - Overloaded operators do not return anything just yet")
+    def test_float_operator_overload_correctness(self, clazz: type):
+        value_a = TestFloatClassicalSyntax._get_float_from_immediate(clazz, 2)
+        value_b = TestFloatClassicalSyntax._get_float_from_immediate(clazz, 6)
         value_c = value_a + value_b
         value_d = value_b + value_a
         value_e = value_b - value_a
