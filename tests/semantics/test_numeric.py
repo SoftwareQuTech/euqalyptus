09606cf5799345cbead56e1a7bc99ed0afd53471
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-02-15 17:54:03 -0300

    Split array test sinto its separate file

--- tests/semantics/test_numeric.py
+++ tests/semantics/test_numeric.py
@@ -1,12 +1,8 @@
-from queue import Queue
-from typing import Union, Tuple, Type, List
-
 import pytest
 
 from qoala.ast.operations.numeric import Add, Subtract, Multiply, Divide
-from qoala.ast.value import QoalaInteger, QoalaFloat, Signedness, QoalaArray
+from qoala.ast.value import QoalaInteger, QoalaFloat, Signedness
 from qoala.types.classical import InvalidArgumentError
-from qoala.types.classical.arrays import IntArray, FloatArray
 from qoala.types.classical.floats import Float
 from qoala.types.classical.integer import Int32, UInt32
 
@@ -88,49 +84,3 @@ class TestNumbersSemantics:
         with pytest.raises(InvalidArgumentError) as ex:
             _ = UInt32(-3.25)
         assert str(ex.value) == "'UInt32' type only supports integer values"
-
-
-class TestArraySemantics:
-    arrays_test_data = [
-        ((10, 20), (5, 3), int, Int32, IntArray, QoalaInteger),
-        ((15.3, 10), (-5.8, 4.1), float, Float, FloatArray, QoalaFloat)
-    ]
-
-    @pytest.mark.parametrize("values, constants, vals_type, base_type, array_type, member_type", arrays_test_data)
-    def test_array_semantics(
-            self,
-            values: Union[Tuple[int], Tuple[float]],
-            constants: Union[Tuple[int], Tuple[float]],
-            vals_type: Type,
-            base_type: Type,
-            array_type: Type,
-            member_type: Union[QoalaInteger, QoalaFloat]
-    ):
-        array_values: List[member_type, vals_type] = []
-        in_order_values: Queue[vals_type] = Queue()
-        for value in values:
-            array_values.append(base_type(value))
-            in_order_values.put(value)
-
-        for constant in constants:
-            array_values.append(constant)
-            in_order_values.put(constant)
-
-        array = array_type(*array_values)
-
-        assert isinstance(array, QoalaArray)
-        assert array.length == 4
-        assert array.base_size == 32
-        assert array.base_type == vals_type
-
-        for member in array.members:
-            assert isinstance(member, member_type)
-
-            # We assert the _order_ of the values on the list
-            current_expected_value = in_order_values.get()
-            # An AssertError on this line means that the build of the array
-            # is not correct
-            assert member.value == current_expected_value
-
-            # We don't need to assert the internals of each member, since that
-            # is covered by other tests in this file
