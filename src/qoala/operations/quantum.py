7cd89886872f8cdbcccd4b2a96b86791986a64e1
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2024-03-26 14:03:34 +0100

    Add SendInt SendFloat classes

--- src/qoala/operations/quantum.py
+++ src/qoala/operations/quantum.py
@@ -26,3 +26,31 @@ class RecvFloat(QoalaFloatingPointType):
 
 
 recv_float = RecvFloat
+
+
+# TODO - Inherit from what?
+class SendInt:
+    def __new__(cls, remote_name: str, *args, **kwargs):
+        # TODO
+        pass
+
+    def __init__(self, remote_name: str):
+        # Nothing to do here
+        pass
+
+
+send_int = RecvInt
+
+
+# TODO - Inherit from what?
+class RecvFloat:
+    def __new__(cls, remote_name: str, *args, **kwargs):
+        # TODO
+        pass
+
+    def __init__(self, remote_name: str):
+        # Nothing to do here
+        pass
+
+
+send_float = RecvFloat
