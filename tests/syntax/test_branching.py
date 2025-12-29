2fccfc4214f616ab4bf2a7e374eefa5e916adb1d
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2025-12-29 14:39:27 +0100

    Add first basic test case for syntax checking of qoala booleans.

---
+++ tests/syntax/test_branching.py
@@ -0,0 +1,24 @@
+import pytest
+
+from qoala import QoalaExpression
+from qoala.types.classical.booleans import Bool
+import qoala.utils.debug_info as dbg_info
+
+
+class TestBranchingSyntax:
+
+    @pytest.fixture(autouse=True, scope="function")
+    def setup_debug_info(self, request):
+        # For allowing debug info
+        # Nuance; parametrized tests use [param-types]... remove that part
+        if "[" in request.node.name:
+            bracket_index = request.node.name.index("[")
+            dbg_info.function_name = request.node.name[0:bracket_index]
+        else:
+            dbg_info.function_name = request.node.name
+    def test_direct_boolean_creation(self):
+        bool_true = Bool(True)
+        bool_false = Bool(False)
+
+        assert isinstance(bool_true, QoalaExpression)
+        assert isinstance(bool_false, QoalaExpression)
