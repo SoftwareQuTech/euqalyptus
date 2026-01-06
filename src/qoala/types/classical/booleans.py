44f66719f2c376b3b797801f3f6a9ea921b4973b
Diego Rivera <d.a.g.a.rivera@tudelft.nl>
2026-01-06 13:00:19 +0100

    Fix import of "Self" from typing extensions to keep python 3.10 backwards compatibility

--- src/qoala/types/classical/booleans.py
+++ src/qoala/types/classical/booleans.py
@@ -1,4 +1,5 @@
-from typing import Optional, Self
+from typing_extensions import Self
+from typing import Optional
 
 from qoala.ast.value import QoalaBool
 from qoala.errors import NotBooleanArgumentError
